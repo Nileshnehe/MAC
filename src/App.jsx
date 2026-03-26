20f3dbc02be922fc6b23b56591a2bcf0911841fc
Nileshnhe <nehenilesh111@gmail.com>
2026-03-26 23:00:57 +0530

    feat(ui): create macOS-style draggable window with control dots

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import './app.scss'
 import Doc from './components/Doc'
 import Nav from './components/Nav'
+import MacWindow from './components/Windows/MacWindow'
 
 const App = () => {
   return (
     <main>
       <Nav />
       <Doc />
+
+      <MacWindow>
+
+      </MacWindow>
     </main>
   )
 }
---
+++ src/components/Windows/MacWindow.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Rnd } from 'react-rnd'
+import "./macwindow.scss"
+const MacWindow = ({children}) => {
+  return (
+    <Rnd>
+        <div className='window'>
+            <div className="nav">
+                <div className="dots">
+                    <div className="dot red"></div>
+                    <div className="dot yellow"></div>
+                    <div className="dot green"></div>
+                </div>
+
+                <div className="title">
+                    <p>neelceo - zsh</p>
+                </div>
+            </div>
+
+            <div className="main-content">
+            {children}
+            </div>
+        </div>
+    </Rnd>
+  )
+}
+
+export default MacWindow
